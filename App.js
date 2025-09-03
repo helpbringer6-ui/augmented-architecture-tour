236ef8d5f87d5bfa670ada5cd75a3d07eed1cbbf
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-03 12:05:36 -0500

    feat: initial commit for App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Augmented Architecture Tour</h1>
+        <p>Willkommen zur interaktiven Tour durch historische Gebäude mit Augmented Reality!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
